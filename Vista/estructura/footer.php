53fd629e6f79de80d53795426b1d9f5982def0e0
DiegoMarchandon <diegomarc2020@gmail.com>
2024-11-08 16:26:44 -0300

    agrego estructuras + css + js + pagina  de vista cliente

---
+++ Vista/estructura/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <footer class="bg-light text-center py-4 mt-4 fixed-bottom">
+        <p>&copy; 2024 E-Commerce. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
---
+++ Vista/estructura/header.php
@@ -0,0 +1,40 @@
+<?php
+include_once("../../configuracion.php");
+// $objSession = new Session();
+// $usuario = $objSession->getUsuario(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery-3.7.1.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">comahu<p class="text-primary d-inline">e-shop</p></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Carrito</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
