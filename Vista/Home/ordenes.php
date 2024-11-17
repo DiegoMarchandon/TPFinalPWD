53a414f28c6aef373dbb59f08ddfb09d5a04893f
userAndres99 <andresmenard.arg99@gmail.com>
2024-11-17 20:25:54 -0300

    boton enviar cancelado si el stock no es suficiente

--- Vista/Home/ordenes.php
+++ Vista/Home/ordenes.php
@@ -30,14 +30,14 @@ $ABMCompraItem = new ABMCompraItem();
                                 <?php foreach ($compraItems as $item): ?>
                                     <li>
                                         Producto: <?php echo htmlspecialchars($item->getObjProducto()->getPronombre()); ?>,
-                                        Cantidad: <?php echo htmlspecialchars($item->getCicantidad()); ?>
+                                        Cantidad: <span class="cantidad"><?php echo htmlspecialchars($item->getCicantidad()); ?></span>
                                         <br>
-                                        <span style="color: red;">Stock disponible: <?php echo htmlspecialchars($item->getObjProducto()->getProcantstock()); ?></span>
+                                        <span style="color: red;">Stock disponible: <span class="stock-disponible"><?php echo htmlspecialchars($item->getObjProducto()->getProcantstock()); ?></span></span>
                                     </li>
                                 <?php endforeach; ?>
                             </ul>
                             <div class="text-center mt-3">
-                                <form action="../Action/actionEnviarCompra.php" method="POST" class="d-inline">
+                                <form action="../Action/actionEnviarCompra.php" method="POST" class="d-inline formEnviarCompra">
                                     <input type="hidden" name="idcompra" value="<?php echo htmlspecialchars($compra->getIdcompra()); ?>">
                                     <button type="button" class="btn btn-success btnEnviarCompra">Enviar Compra</button>
                                 </form>
@@ -60,38 +60,41 @@ $ABMCompraItem = new ABMCompraItem();
 
 <script>
     $(document).ready(function () {
+        $('.formEnviarCompra').each(function () {
+            var form = $(this);
+            var stockInsuficiente = false;
+
+            // Verificar el stock disponible antes de habilitar el botón
+            form.closest('.card').find('ul li').each(function () {
+                var cantidad = parseInt($(this).find('.cantidad').text());
+                var stockDisponible = parseInt($(this).find('.stock-disponible').text());
+                if (cantidad > stockDisponible) {
+                    stockInsuficiente = true;
+                    return false; // Salir del bucle
+                }
+            });
+
+            if (stockInsuficiente) {
+                form.find('.btnEnviarCompra').prop('disabled', true);
+                form.find('.btnEnviarCompra').after('<p class="text-danger">Debe renovar stock para poder enviar esta compra</p>');
+            }
+        });
+
         $('.btnEnviarCompra').click(function (event) {
             event.preventDefault();
-            // var idCompra = $(this).closest('form').data('id');
             var idCompra = $(this).closest('form').find('input[name="idcompra"]').val();
-            console.log("idCompra: "+idCompra);
             $.ajax({
                 url: '../Action/actionEnviarCompra.php',
                 method: 'POST',
                 data: { idcompra: idCompra },
-                // jQuery detecta que la respuesta del servidor tiene el tipo application/json en su encabezado, por lo que parsea automáticamente el JSON recibido en un objeto javascript
-                /* success: function(response){
-                    console.log(response);
-                    // console.log("response");
-                } */
                 success: function (response) {
                     response = typeof response === 'string' ? JSON.parse(response) : response;
-                    // console.log(response);
-                    console.log(response);
-                    alert("alertando respuesta exitosa");
-                    // response = typeof response === 'string' ? JSON.parse(response) : response;
                     if(response.status === 'success'){
-                        console.log("response succes");
-                        alert('Compra enviada: ');
-                        // window.location.href = 'ordenes.php';
+                        alert('Compra enviada');
                         window.location.href = response.redirect;
                     }else{
-                        alert('Compra no enviada: ');
-                        console.log("response error");
+                        alert(response.message);
                     }
-
-                    // Manejo de la respuesta
-                    // Aquí puedes agregar lógica adicional para actualizar la página si es necesario
                 },
                 error: function (xhr, status, error) {
                     alert('Error al enviar la compra: ' + error);
@@ -100,21 +103,19 @@ $ABMCompraItem = new ABMCompraItem();
         });
 
         $('.btnCancelarCompra').click(function () {
-            var idCompra = $(this).closest('form').data('id');
+            var idCompra = $(this).closest('form').find('input[name="idcompra"]').val();
             $.ajax({
                 url: '../Action/actionCancelarCompra.php',
                 method: 'POST',
                 data: { idcompra: idCompra },
                 success: function (response) {
-                    // Manejo de la respuesta
                     response = typeof response === 'string' ? JSON.parse(response) : response;
                     window.location.href = 'ordenes.php';
                     if(response.status === 'success'){
-                        alert('Compra enviada');
+                        alert('Compra cancelada');
                     }else{
-                        alert('la Compra no se ha podido enviar ');
+                        alert('La compra no se ha podido cancelar');
                     }
-                    // Actualizar la página si es necesario
                 },
                 error: function (xhr, status, error) {
                     alert('Error al cancelar la compra: ' + error);
