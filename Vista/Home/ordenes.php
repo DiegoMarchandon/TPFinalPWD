9e48a15dba7b4398208f35646ef985878b9d0028
userAndres99 <andresmenard.arg99@gmail.com>
2024-11-19 15:17:37 -0300

    comento el sendemail para no estar enviando mails todo el rato

--- Vista/Home/ordenes.php
+++ Vista/Home/ordenes.php
@@ -100,7 +100,7 @@ $ABMCompraItem = new ABMCompraItem();
                         var toName = response.toName;
                         var toEmail = response.toEmail;
                         var message = 'Su compra ha sido enviada con éxito. Gracias por su compra.';
-                        sendEmail(toName, toEmail, message);
+//---------------------- sendEmail(toName, toEmail, message);
                         alert('Compra enviada con éxito. Se ha enviado un correo de confirmación.');
                         window.location.href = response.redirect;
                     }else{
@@ -132,7 +132,7 @@ $ABMCompraItem = new ABMCompraItem();
                         var toName = response.toName;
                         var toEmail = response.toEmail;
                         var message = 'Su compra ha sido cancelada. Si tiene alguna pregunta, por favor contáctenos.';
-                        sendEmail(toName, toEmail, message);
+//---------------------- sendEmail(toName, toEmail, message);
                         alert('Compra cancelada con éxito. Se ha enviado un correo de confirmación.');
                         window.location.href = response.redirect;
                     } else {
